f7ca68392cf563a45b0f9ac820395ed0addc6aaa
takikomiudon <takikomiudon1743@gmail.com>
2024-02-26 01:34:14 +0900

    next.js, zod, supabase, prisma

---
+++ app/bbs-posts/create/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+import { postBBS } from '@/app/actions/postBBSAction';
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+export const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: 'ユーザー名は2文字以上で入力してください' }),
+  title: z
+    .string()
+    .min(2, { message: 'タイトルは2文字以上で入力してください' }),
+  content: z
+    .string()
+    .min(10, { message: '本文は10文字以上で入力してください' })
+    .max(140, { message: '本文は140文字以内で入力してください' }),
+});
+
+const CreateBBSPage = () => {
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      title: '',
+      content: '',
+    },
+  });
+
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    const { username, title, content } = value;
+    postBBS({ username, title, content });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3 w-1/2 px-7">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="usename" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea placeholder="content" className='resize-none' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default CreateBBSPage;
---
+++ app/components/BBSCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
+import Link from 'next/link';
+import { BBSData } from '../types/types';
+
+interface BBSCardProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({bbsData}: BBSCardProps) => {
+  const { id, title, content, createdAt, username } = bbsData;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {content}
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href={`/bbs-posts/${id}`} className='text-blue-500'>
+          Read More
+        </Link>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default BBSCard
